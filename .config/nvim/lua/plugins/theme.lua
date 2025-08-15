85d4e85495b58de01d62d0544902c9af2da82a1c
Ryan Collins <ry@rycollins.com>
2025-08-15 11:23:30 -0400

    nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,6 +1,12 @@
 return {
   "craftzdog/solarized-osaka.nvim",
   lazy = false,
-  priority = 1000,
+  priority = 10,
   opts = {},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
 }
