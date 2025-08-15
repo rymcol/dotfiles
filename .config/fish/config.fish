b418b55b320837b1df7dac5d0c2a16b9f1bc51c4
Ryan Collins <ry@rycollins.com>
2025-08-15 07:28:56 -0400

    tide configure

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -84,3 +84,6 @@ set -gx IT_TERMINAL_PROMPT 1
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# init correct tide config
+tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Dotted --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Compact --icons='Many icons' --transient=Yes
