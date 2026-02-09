564afe48c51321fef0fd5174a5f9df45731db71f
Ryan Collins <ry@rycollins.com>
2026-02-09 15:54:44 -0500

    niri setup

--- .config/fish/functions/set-init4.fish
+++ .config/fish/functions/set-init4.fish
@@ -1,14 +1,21 @@
 function set-init4 -a env
     if test "$env" != "dev" -a "$env" != "prod"
-        echo "Usage: switch_env dev|prod"
+        echo "Usage: set-init4 dev|prod"
         return 1
     end
+
     set -gx AWS_PROFILE $env
+
     aws sts get-caller-identity --profile $AWS_PROFILE > /dev/null 2>&1
-    if test $status -ne 0
-        aws sso login --profile $AWS_PROFILE
-    end
+    or aws sso login --profile $AWS_PROFILE
+
     set -gx PULUMI_CONFIG_PASSPHRASE_FILE /Users/ryan/.pulumi/config.$env
-    aws eks update-kubeconfig --name the-$env-cluster --profile $AWS_PROFILE
-    kubectl config use-context $env-cluster
+
+    if test "$env" = "dev"
+        aws eks update-kubeconfig --name the-dev-cluster --profile $AWS_PROFILE --region us-east-1
+        kubectl config use-context arn:aws:eks:us-east-1:637423570300:cluster/the-dev-cluster
+    else
+        aws eks update-kubeconfig --name the-prod-cluster --profile $AWS_PROFILE --region us-east-1
+        kubectl config use-context arn:aws:eks:us-east-1:381492309153:cluster/the-prod-cluster
+    end
 end
